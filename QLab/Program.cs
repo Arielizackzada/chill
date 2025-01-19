c4e50beedd0d8432788e27a9797d3f0fc9adb848
Arielizackzada <160001718+Arielizackzada@users.noreply.github.com>
2025-01-19 10:11:10 +0200

    Add files via upload

--- QLab/Program.cs
+++ QLab/Program.cs
@@ -1,6 +1,7 @@
-﻿namespace QLab;
+﻿
+namespace QLab;
 public class Program
-// אריאל יהודה יצחק זדה
+    // אריאל יהודה יצחק זדה  - 19.1.25
 
 {
     public static void Main(string[] args)
@@ -14,30 +15,38 @@ public class Program
         Queue<int> qCopy = SetQCopy(q);
         Console.WriteLine(q);
         Console.WriteLine(qCopy);
-        q.Insert(17);
+        q.Insert(7);
         qCopy.Insert(13);
+        q.Insert(14);
+        q.Insert(7);
+        q.Insert(20);
+
+        Queue<int> q2 = new Queue<int>();
+
+        q2.Insert(1);
+        q2.Insert(3);
+        q2.Insert(1);
+        q2.Insert(-1);
+
+        bool checkPerfect = CheckQPerfect(q2);
+        Console.WriteLine(checkPerfect);
+        Console.WriteLine(q2);
+
+        bool check = CheckAround(q, 5);
+
+        Console.WriteLine(check);
+
         Console.WriteLine(q);
         Console.WriteLine(qCopy);
+        Queue<int> q3 = new Queue<int>();
+        q3.Insert(7);
+        q3.Insert(10);
+        q3.Insert(17);
+        q3.Insert(20);
+        Queue<int> newQ = OrderQueue(q3, 5);
+        Console.WriteLine(newQ);
+    }
 
-        bool isIn = IfNumInQ(q,3);
-        Console.WriteLine(isIn);
-    }
-    public static bool IfNumInQ(Queue<int> q,int num) 
-    // הפונקציה מחזירה אם המספר נמצא בתור
-    {
-        int ezer = 0;
-        bool numInQ = false;
-        Queue<int> qCopy = SetQCopy(q);
-        while (!qCopy.IsEmpty()) 
-        {
-            ezer = qCopy.Remove();
-            if (num == ezer) 
-            {
-                numInQ =  true;
-            }
-        }
-        return numInQ;
-    }
 
     public static Queue<int> SetQCopy(Queue<int> q)
     {
@@ -58,6 +67,100 @@ public class Program
         }
         return qCopy;
     }
-        
 
+    public static bool CheckAround(Queue<int> q, int place)
+    {
+
+        bool check = false;
+        int right = 0;
+        int sum = 0;
+        int current = 0;
+        Queue<int> qCopy = SetQCopy(q);
+        Queue<int> qCopy2 = SetQCopy(q);
+        int count = 0;
+
+        while (!qCopy2.IsEmpty())
+        {
+            count++;
+            qCopy2.Remove();
+        }
+
+        if (place < count && place > 1)
+        {
+            for (int i = 1; i < place; i++)
+            {
+                right = qCopy.Remove();
+            }
+
+
+            current = qCopy.Remove();
+            sum = qCopy.Remove() + right;
+
+            if (current == sum)
+            {
+                check = true;
+            }
+        }
+
+        return check;
+    }
+
+    public static bool CheckQPerfect(Queue<int> q)
+    {
+        Queue<int> qCopy = SetQCopy(q);
+        int length = 0;
+        int count = 0;
+
+        while (!qCopy.IsEmpty())
+        {
+            length++;
+            qCopy.Remove();
+        }
+
+        for (int place = 2; place < length; place++)
+        {
+            if (CheckAround(q, place))
+            {
+                count++;
+            }
+        }
+        if (count == length - 2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+
+    }
+    public static Queue<int> OrderQueue(Queue<int> q,int numToPut) 
+    {
+        Queue<int> qCopy = SetQCopy(q);
+        Queue<int> queue = new Queue<int>();
+        bool checkIfPut = false;
+        int currentNum = 0;
+        while (!qCopy.IsEmpty()) 
+        {
+            currentNum = qCopy.Remove();
+            if ((currentNum > numToPut) && (checkIfPut = false))
+            {
+                queue.Insert(numToPut);
+                queue.Insert(currentNum);
+                checkIfPut = true;
+                Console.WriteLine("bbbbbbbbbb");
+            }
+            else 
+            {
+               queue.Insert(currentNum);
+                Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxx");
+            }
+        }
+        if (!checkIfPut) 
+        {
+            queue.Insert(numToPut);
+        }
+        return queue;
+    }
 }
