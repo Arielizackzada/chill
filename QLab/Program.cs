1427f22c0e7b225780a244e2d4cb6f627d19f1ff
Arielizackzada <160001718+Arielizackzada@users.noreply.github.com>
2025-01-23 09:58:56 +0200

    Add files via upload

--- QLab/Program.cs
+++ QLab/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace QLab;
 public class Program
-    // אריאל יהודה יצחק זדה  - 19.1.25
+// אריאל יהודה יצחק זדה  - 19.1.25
 
 {
     public static void Main(string[] args)
@@ -43,16 +43,25 @@ public class Program
         q3.Insert(10);
         q3.Insert(17);
         q3.Insert(20);
-        Queue<int> newQ = OrderQueue(q3, 5);
-        Console.WriteLine(newQ);
+        OrderQueue(q3, 5);
+        Console.WriteLine(q3);
+        Student s1 = new("Eyal", 45);
+        Student s2 = new("Lior", 85);
+        Student s4 = new ("Ariel",96);
+        Student s3 = new("Roei", 87);
+        Queue<Student> q4 = new Queue<Student>();
+        q4.Insert(s1);
+        q4.Insert(s2);
+        q4.Insert(s3);
+        Console.WriteLine(Studentq(q4,s4));
     }
 
 
-    public static Queue<int> SetQCopy(Queue<int> q)
+    public static Queue<T> SetQCopy<T>(Queue<T> q)
     {
-        Queue<int> qCopy = new();
-        Queue<int> qTemp = new();
-        int currItem;
+        Queue<T> qCopy = new();
+        Queue<T> qTemp = new();
+        T currItem;
         //פריקת התור המקורי ויצירת תור העתק ותור זמני
         while (!q.IsEmpty())
         {
@@ -69,6 +78,7 @@ public class Program
     }
 
     public static bool CheckAround(Queue<int> q, int place)
+    //פונקציה שבודקת האם המספר במקום הנתון בתור הוא סכום של שני המספרים שמסביב לו
     {
 
         bool check = false;
@@ -106,6 +116,7 @@ public class Program
     }
 
     public static bool CheckQPerfect(Queue<int> q)
+    //פונקציה שבודקת האם כל המספרים בתור הם סכום של שני המספרים שמסביב להם
     {
         Queue<int> qCopy = SetQCopy(q);
         int length = 0;
@@ -135,13 +146,14 @@ public class Program
 
 
     }
-    public static Queue<int> OrderQueue(Queue<int> q,int numToPut) 
+    public static void OrderQueue(Queue<int> q, int numToPut)
+    //הפונקציה מקבלת תור מסודר ומספר ומשנה את התור כך שהמספר ייכנס לתור במקום הנכון
     {
         Queue<int> qCopy = SetQCopy(q);
         Queue<int> queue = new Queue<int>();
         bool checkIfPut = false;
         int currentNum = 0;
-        while (!qCopy.IsEmpty()) 
+        while (!qCopy.IsEmpty())
         {
             currentNum = qCopy.Remove();
             if ((currentNum > numToPut) && (checkIfPut = false))
@@ -149,18 +161,39 @@ public class Program
                 queue.Insert(numToPut);
                 queue.Insert(currentNum);
                 checkIfPut = true;
-                Console.WriteLine("bbbbbbbbbb");
             }
-            else 
+            else
             {
-               queue.Insert(currentNum);
-                Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxx");
+                queue.Insert(currentNum);
             }
         }
-        if (!checkIfPut) 
+        if (!checkIfPut)
+        {
+            queue.Insert(numToPut);
+        }
+        q = queue;
+    }
+    public static Queue<Student> Studentq(Queue<Student> q, Student numToPut) 
+    {
+        Queue<Student> qCopy = SetQCopy(q);
+        Queue<Student> queue = new Queue<Student>();
+        bool checkIfPut = false;
+        Student currentNum;
+        while (!qCopy.IsEmpty())
+        {
+            currentNum = qCopy.Remove();
+            if ((currentNum.GetGrade() > numToPut.GetGrade()) && (checkIfPut = false))
+            {
+                queue.Insert(numToPut);
+                checkIfPut = true;
+            }
+            queue.Insert(currentNum);
+        }
+        if (!checkIfPut)
         {
             queue.Insert(numToPut);
         }
         return queue;
     }
+    
 }
---
+++ QLab/student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLab
+{
+    public class Student
+    {
+        //מאפייני התלמיד
+        public string name;
+        private int grade;
+        //פונקצייה בונה
+        public Student(string name,int grade)
+        {
+            this.name = name;
+            this.grade = grade;
+        }
+
+        // פונקציית דריסה
+        public override string ToString()
+        {
+            return name + " (" + grade + ")";
+
+        }
+        public int GetGrade()
+        {
+            return grade;
+        }
+
+    }
+}
